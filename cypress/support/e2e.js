ba9916b001f20ce1a6f452c2d2ca5e62e4b5322f
isabellelourenco <isabellelourenco02@gmail.com>
2023-08-05 13:03:26 -0300

    add cypress-axe import

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands/api_commands'
 import './commands/ui_commands'
+import 'cypress-axe'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')

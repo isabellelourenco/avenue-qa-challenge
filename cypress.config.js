07f97fcc2d389daf11e918b0c4c59afe917aabfe
isabellelourenco <isabellelourenco02@gmail.com>
2023-08-05 13:03:03 -0300

    include new libs and setup configurations

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "16a646",
   e2e: {
-    baseUrl: 'https://www.amazon.com.br',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    env: {
+      API_URL: 'https://api.openweathermap.org/data/2.5/weather',
+      requestMode: true,
     },
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        }
+      })
+    },
+    viewportWidth: 1920,
+    viewportHeight: 1080
   },
 });

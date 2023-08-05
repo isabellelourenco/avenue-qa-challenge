1935f12bdf2f78af97637fb9e7d75c3f578f05ab
isabellelourenco <isabellelourenco02@gmail.com>
2023-08-05 15:20:13 -0300

    refactoring create account test for avenue page

--- cypress/e2e/ui/avenue/createAccount.cy.js
+++ cypress/e2e/ui/avenue/createAccount.cy.js
@@ -1,8 +1,11 @@
 describe('Create account', () => {
 
+    beforeEach(() => {
+        cy.visit('https://avenue.us/')
+    })
+
     it('should access create account page', () => {
         
-        cy.visit('https://avenue.us/')
         cy.get('#header_create_account').click()
         cy.get('#IdentityForm').should('be.visible')
     });
@@ -10,7 +13,6 @@ describe('Create account', () => {
     it('should access login page', () => {
         const text = 'Acesse sua conta'
 
-        cy.visit('https://avenue.us/')
         cy.get('#header_account_login').click()
         cy.get('.form-signin h3').should('have.text',text)
         cy.url().should('eq','https://pit.avenue.us/login')

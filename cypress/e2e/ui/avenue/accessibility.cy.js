fc516eaae9c7c044e339c08e5a8f33c62ad8c8df
isabellelourenco <isabellelourenco02@gmail.com>
2023-08-06 09:30:38 -0300

    include tag accessibility

--- cypress/e2e/ui/avenue/accessibility.cy.js
+++ cypress/e2e/ui/avenue/accessibility.cy.js
@@ -1,6 +1,6 @@
 
 describe('Accessibility Tests', () => {
-    it('should log failures with serious and critical impact on the home page', () => {
+    it('should log failures with serious and critical impact on the home page', { tags: '@accessibility' }, () => {
         cy.visit('https://avenue.us/')
         cy.injectAxe({ axeCorePath: 'node_modules/axe-core/axe.min.js' })
         cy.checkA11y()
@@ -9,7 +9,7 @@ describe('Accessibility Tests', () => {
           });
     })
 
-    it('should log failures with serious and critical impact on the login page', () => {
+    it('should log failures with serious and critical impact on the login page', { tags: '@accessibility' }, () => {
         cy.visit('https://pit.avenue.us/login')
         cy.injectAxe({ axeCorePath: 'node_modules/axe-core/axe.min.js' })
         cy.checkA11y()
@@ -18,7 +18,7 @@ describe('Accessibility Tests', () => {
           });
     })
 
-    it('should log failures with serious and critical impact on the registry page', () => {
+    it('should log failures with serious and critical impact on the registry page', { tags: '@accessibility' }, () => {
         cy.visit('https://pit.avenue.us/register/identity-info')
         cy.injectAxe({ axeCorePath: 'node_modules/axe-core/axe.min.js' })
         cy.checkA11y()

412532f69470dfe2633caaf6ed3f0e2befbc6f4c
isabellelourenco <isabellelourenco02@gmail.com>
2023-08-06 09:20:49 -0300

    Adjustments

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   projectId: "k5pkre",
   e2e: {
     env: {
+      hideXhr: true,
       API_URL: 'https://api.openweathermap.org/data/2.5/weather',
       requestMode: true,
     },
